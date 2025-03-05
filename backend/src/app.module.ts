c8b3aecf27117ca676b9afcdb365bbd9a1d4dcd6
wakamebu <wakamebu3@gmail.com>
2025-03-05 18:06:53 +0900

    setup:gql

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,21 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDrive , ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { TaskModule } from './task/task.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground : true,
+      autoSchemaFile: join(process.cwd(), 'src/cshema.gql'),
+      cors:{
+        // 全て許可
+        origin: '*', 
+      },
+    }),
+    TaskModule,
+  ],
 })
 export class AppModule {}

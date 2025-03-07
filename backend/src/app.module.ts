b76b1d930e7e7d5b89b13e558af3846aa4d2f63f
wakamebu <wakamebu3@gmail.com>
2025-03-07 15:20:02 +0900

    add:user作成機能

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,6 +4,7 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
 import { TaskModule } from './task/task.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -18,6 +19,7 @@ import { PrismaModule } from './prisma/prisma.module';
     }),
     TaskModule,
     PrismaModule,
+    UserModule,
   ],
 })
 export class AppModule {}
---
+++ backend/src/user/user.resolver.ts
@@ -0,0 +1,15 @@
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { UserService } from './user.service';
+import { CreateUserInput } from './dto/createUser.input';
+import { User } from '@prisma/client';
+import { User as UserModel } from './models/user.model';
+
+@Resolver()
+export class UserResolver {
+    constructor(private readonly userService: UserService){}
+
+    @Mutation(() =>　UserModel)
+    async createUser(@Args('createUserInput') createUserInput: CreateUserInput): Promise<User>{
+        return await this.userService.createUser(createUserInput);
+    }
+}

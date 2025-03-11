5eae84cdb29c9f68ed68f36253f4f8cc711f58e9
wakamebu <wakamebu3@gmail.com>
2025-03-11 14:30:14 +0900

    add:SignInPage

--- frontend/src/apolloClient.ts
+++ frontend/src/apolloClient.ts
@@ -1,8 +1,10 @@
+//ApolloClient → GraphQL サーバーとやり取りするためのメインクラス
+//setContext → リクエストの前にヘッダー情報を変更するための関数
 import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
-  uri: 'hattp://localhost:3000/graphql'
+  uri: 'http://localhost:3000/graphql'
 });
 
 const authLink = setContext((_, prevContext ) => {
@@ -10,13 +12,15 @@ const authLink = setContext((_, prevContext ) => {
   return {
     headers: {
       ...prevContext.headers,
-      authorization: token? `Bearer ${token}` : ''
+      authorization: token ? `Bearer ${token}` : ''
     }
   }
 });
 
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
+
+  //毎回 API にリクエストさせない
   cache: new InMemoryCache(),
 })
 

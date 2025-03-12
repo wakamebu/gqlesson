3a69c1b176d1799e9e32c16fb0999e6df6a29eba
wakamebu <wakamebu3@gmail.com>
2025-03-12 15:51:33 +0900

    add:タスク編集機能

--- frontend/src/components/TaskTable.tsx
+++ frontend/src/components/TaskTable.tsx
@@ -6,6 +6,7 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { Task } from '../types/task';
+import EditTask from './EditTask';
 
 // function createData(name: string, dueDate: string, status: string) {
 //   return { name, dueDate, status };
@@ -42,6 +43,7 @@ export default function TaskTable({ tasks, userId }: TaskTableProps) {
               </TableCell>
               <TableCell align="right">{task.dueDate}</TableCell>
               <TableCell align="right">{task.status}</TableCell>
+              <TableCell align="right"><EditTask task={task} userId={userId}/></TableCell>
             </TableRow>
           ))}
         </TableBody>
--- frontend/src/mutations/taskMutations.ts
+++ frontend/src/mutations/taskMutations.ts
@@ -2,12 +2,24 @@ import { gql } from '@apollo/client';
 
 export const CREATE_TASK = gql`
   mutation createTask($createTaskInput: CreateTaskInput!) {
-   createTask(createTaskInput: $createTaskInput){
-    id
-		name
-    dueDate
-    status
-    description
+    createTask(createTaskInput: $createTaskInput) {
+      id
+      name
+      dueDate
+      status
+      description
+    }
   }
-}
+`;
+
+export const UPDATE_TASK = gql`
+  mutation updateTask($updateTaskInput: UpdateTaskInput!) {
+    updateTask(updateTaskInput: $updateTaskInput) {
+      id
+      name
+      dueDate
+      status
+      description
+    }
+  }
 `;
